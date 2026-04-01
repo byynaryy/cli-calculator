02b57b590fbb9db352581790393096e6d6afa480
Jeff <jeff@ip-192-168-1-122.eu-west-3.compute.internal>
2026-04-01 22:48:56 +0200

    chore: add read input

--- src/com/oomph/calculator/Main.java
+++ src/com/oomph/calculator/Main.java
@@ -2,6 +2,7 @@ package com.oomph.calculator;
 
 public class Main {
     static void main() {
-
+        String calculation = IO.readln("Enter your calculation: ");
+        System.out.println(calculation);
     }
 }
