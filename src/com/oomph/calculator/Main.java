013a6a1019989e55b66327f8ff26e79692e7bcb7
jeff <endeki@gmail.com>
2026-04-02 22:49:01 +0200

    chore: update the prompt

--- src/com/oomph/calculator/Main.java
+++ src/com/oomph/calculator/Main.java
@@ -2,7 +2,7 @@ package com.oomph.calculator;
 
 public class Main {
     static void main() {
-        String calculation = IO.readln("Calculate: ");
+        String calculation = IO.readln("Enter your calculation: ");
         System.out.println(calculation);
     }
 }
