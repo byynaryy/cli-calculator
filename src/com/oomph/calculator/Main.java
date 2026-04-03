00e9f30fe8e4248a251f54eadbd9ea507efb6daa
jeff <endeki@gmail.com>
2026-04-03 18:05:22 +0200

    chore: split calculation expression

--- src/com/oomph/calculator/Main.java
+++ src/com/oomph/calculator/Main.java
@@ -1,8 +1,34 @@
 package com.oomph.calculator;
 
+
 public class Main {
     static void main() {
         String calculation = IO.readln("Enter your calculation: ");
         System.out.println(calculation);
+        String[]  items = calculation.split("(?<=[+\\-*/])|(?=[+\\-*/])");
+        int a = Integer.parseInt(items[0]);
+        String op = items[1];
+        int b = Integer.parseInt(items[2]);
+        int result;
+
+        switch (op) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown operator: " + op);
+        }
+
+        System.out.println(result);
+
     }
 }
