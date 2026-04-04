5156af7e430bf0a4cf4fc7e9be8ff048a3a67925
jeff <endeki@gmail.com>
2026-04-04 23:31:02 +0200

    chore: refactor main class

--- src/com/oomph/calculator/Calculator.java
+++ src/com/oomph/calculator/Calculator.java
@@ -1,4 +1,30 @@
 package com.oomph.calculator;
 
 public class Calculator {
+    public static void calculate(String calculation) {
+        String[] items = calculation.split("(?<=[+\\-*/])|(?=[+\\-*/])");
+        int a = Integer.parseInt(items[0].trim());
+        String op = items[1].trim();
+        int b = Integer.parseInt(items[2].trim());
+
+        int result;
+
+        switch (op) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown operator: " + op);
+        }
+        System.out.println(result);
+    }
 }
--- src/com/oomph/calculator/Main.java
+++ src/com/oomph/calculator/Main.java
@@ -4,30 +4,7 @@ package com.oomph.calculator;
 public class Main {
     static void main() {
         String calculation = IO.readln("Enter your calculation: ").trim();
-        String[]  items = calculation.split("(?<=[+\\-*/])|(?=[+\\-*/])");
-        int a = Integer.parseInt(items[0].trim());
-        String op = items[1].trim();
-        int b = Integer.parseInt(items[2].trim());
+        Calculator.calculate(calculation);
 
-        int result;
-
-        switch (op) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown operator: " + op);
-        }
-
-        System.out.println(result);
     }
 }
