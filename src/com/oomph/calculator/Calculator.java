e35574b7c7bb83ea9f67f35e04cd9a229e4079a7
jeff <endeki@gmail.com>
2026-04-05 09:11:13 +0200

    chore: split digits and ops into stacks

--- src/com/oomph/calculator/Calculator.java
+++ src/com/oomph/calculator/Calculator.java
@@ -1,8 +1,24 @@
 package com.oomph.calculator;
 
+import java.util.Stack;
+
 public class Calculator {
     public static void calculate(String calculation) {
         String[] items = calculation.split("(?<=[+\\-*/])|(?=[+\\-*/])");
+
+        Stack<Integer> digits = new Stack<>();
+        Stack<Character> ops = new Stack<>();
+
+        for(int i = 0; i <= items.length; i++) {
+            if(isInt(items[i])) {
+                digits.push(Integer.parseInt(items[i].trim()));
+            } else {
+                ops.push(items[i].trim());
+            }
+        }
+
+
+
         int a = Integer.parseInt(items[0].trim());
         String op = items[1].trim();
         int b = Integer.parseInt(items[2].trim());
@@ -26,5 +42,14 @@ public class Calculator {
                 throw new IllegalArgumentException("Unknown operator: " + op);
         }
         System.out.println(result);
+    }
+
+    private static boolean isInt(String s) {
+        try {
+            Integer.parseInt(s.trim());
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
